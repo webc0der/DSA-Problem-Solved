ff6fd380f86fa8d71c27c9d92c12ee561de5aee0
manish <manish.singh@travelspends.com>
2020-06-10 23:31:58 +0530

    test commit

--- src/main/java/com/Datastructure_Alogorithm/problems_solved/App.java
+++ src/main/java/com/Datastructure_Alogorithm/problems_solved/App.java
@@ -6,13 +6,28 @@ package com.Datastructure_Alogorithm.problems_solved;
  */
 public class App 
 {
-	public static void main( String[] args )
-	{
+	public static void main( String[] args ) {
+		
+		char c = 'A'; //single charecter
+		char c1 = 79; // ACII value
+		char c2 = '\u0675'; //unicode
+		char c3 = '\361'; //octale code
+		char c4 = '\''; //octale code
+		String str = "helo \\all\\";
+		System.out.println(c);
+		System.out.println(c1);
+		System.out.println(c2);
+		System.out.println(c3);
+		System.out.println(str);
+		
+	}
+	
+	public static void changeCase() {
 		String str = "Abc";
 		String result="";
 		char c=' ';
 		for(int i = 0; i <= str.length()-1; i++) {
-			
+
 			if(str.charAt(i) == 'A') {
 				c = (char) (str.charAt(i)+32);
 			}
